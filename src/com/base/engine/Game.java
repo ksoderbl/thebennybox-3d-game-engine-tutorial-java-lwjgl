45fa33f32dd4860cd9251a6cadf12d2f4ced300f
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2019-03-16 15:54:06 +0200

    Tutorial 26: specular reflection

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -38,16 +38,16 @@ public class Game
 		transform = new Transform();
 
 		Vertex[] data = new Vertex[] {
-				new Vertex(new Vector3f(-1f,-1f,0f), new Vector2f(0f,0f)),
-				new Vertex(new Vector3f(0f,1f,0f), new Vector2f(0f,1f)),
-				new Vertex(new Vector3f(1f,-1f,0f), new Vector2f(1.0f,1f)),
-				new Vertex(new Vector3f(0f,-1f,1f), new Vector2f(1f,0f))
+				new Vertex(new Vector3f(-1f,-1f,-1f), new Vector2f(0f,0f)),
+				new Vertex(new Vector3f(1f,-1f,-1f), new Vector2f(0f,1f)),
+				new Vertex(new Vector3f(0f,-1f,1f), new Vector2f(1.0f,1f)),
+				new Vertex(new Vector3f(0f,1f,0f), new Vector2f(1f,0f))
 						};
 		int[] indices = new int[] {
-				3,1,0,
+				0,3,1,
 				2,1,3,
-				0,1,2,
-				0,2,3
+				2,0,1,
+				3,0,2
 		};
 		mesh.addVertices(data, indices, true);
 
--- src/com/base/engine/Material.java
+++ src/com/base/engine/Material.java
@@ -4,11 +4,20 @@ public class Material
 {
     private Texture texture;
     private Vector3f color;
+    private float specularIntensity;
+    private float specularPower; // spread of reflection
 
     public Material(Texture texture, Vector3f color)
     {
+        this(texture, color, 2, 32);
+    }
+
+    public Material(Texture texture, Vector3f color, float specularIntensity, float specularPower)
+    {
         this.texture = texture;
         this.color = color;
+        this.specularIntensity = specularIntensity;
+        this.specularPower = specularPower;
     }
 
     public Texture getTexture()
@@ -29,5 +38,25 @@ public class Material
     public void setColor(Vector3f color)
     {
         this.color = color;
+    }
+
+    public float getSpecularIntensity()
+    {
+        return specularIntensity;
+    }
+
+    public void setSpecularIntensity(float specularIntensity)
+    {
+        this.specularIntensity = specularIntensity;
+    }
+
+    public float getSpecularPower()
+    {
+        return specularPower;
+    }
+
+    public void setSpecularPower(float specularPower)
+    {
+        this.specularPower = specularPower;
     }
 }
