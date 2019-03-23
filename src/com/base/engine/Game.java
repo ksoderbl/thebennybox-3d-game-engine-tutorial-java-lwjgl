587bad9067612cd17a1242212a7a1cebd10b71ef
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2019-03-24 00:05:19 +0200

    Wolfenstein 3D Clone Tutorial: Setting up the renderer

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -16,34 +16,37 @@ import com.base.engine.Vector3f;
 
 public class Game
 {
-	// wolfenstein
-	Bitmap level;
-
+	// wolfenstein 3D
+	private Bitmap level;
+	private Shader shader;
+	private Material material;
+	private Mesh mesh;
+	private Transform transform;
 
 	// tutorial 31
 	private boolean tutorial31stuff = false;
-	private boolean wolfenstein3Dclone = true;
+	private boolean wolfenstein3Dclone = !tutorial31stuff;
 
-	private Mesh mesh;
-	private Shader shader;
-	private Transform transform;
-	private Material material;
-	private Camera camera;
+	private Mesh tutorial31Mesh;
+	private Shader tutorial31Shader;
+	private Transform tutorial31Transform;
+	private Material tutorial31Material;
+	private Camera tutorial31Camera;
 
-	PointLight pLight1 = new PointLight(
+	PointLight tutorial31PLight1 = new PointLight(
 			new BaseLight( new Vector3f(1,0.5f,0), 0.8f ),
 			new Attenuation(0, 0, 1 ),
 			new Vector3f(-2, 0, 5	),
 			10
 	);
-	PointLight pLight2 = new PointLight(
+	PointLight tutorial31PLight2 = new PointLight(
 			new BaseLight( new Vector3f(0,0.5f,1), 0.8f ),
 			new Attenuation(0, 0, 1 ),
 			new Vector3f(2, 0, 7	),
 			10
 	);
 
-	SpotLight sLight1 = new SpotLight(new PointLight(
+	SpotLight tutorial31SLight1 = new SpotLight(new PointLight(
 			new BaseLight( new Vector3f(1,1,1), 1.0f ),
 			new Attenuation(0, 0, 0.1f ),
 			new Vector3f(-2, 0, 5	),
@@ -52,7 +55,8 @@ public class Game
 
 	public Game()
 	{
-		if (wolfenstein3Dclone) {
+		if (wolfenstein3Dclone)
+		{
 			level = new Bitmap("level1.png");
 
 			// This transposes the level, which is not necessarily what we want.
@@ -72,14 +76,38 @@ public class Game
 				}
 				System.out.println();
 			}
+
+			shader = BasicShader.getInstance();
+			material = new Material(new Texture("test.png"), new Vector3f(1, 1, 1));
+
+			Vertex[] vertices = new Vertex[]{
+					new Vertex(new Vector3f(0, 0, 0), new Vector2f(0, 0)),
+					new Vertex(new Vector3f(0, 1, 0), new Vector2f(0, 1)),
+					new Vertex(new Vector3f(1, 1, 0), new Vector2f(1, 1)),
+					new Vertex(new Vector3f(1, 0, 0), new Vector2f(1, 0))
+			};
+			int[] indices = new int[] {
+				0, 1, 2,
+				0, 2, 3
+			};
+
+			mesh = new Mesh(vertices, indices);
+			transform = new Transform();
+			Transform.setCamera(new Camera());
+			transform.setProjection(70.0f, Window.getWidth(), Window.getHeight(), 0.01f, 1000f);
+
+			System.out.println("=====================================================================");
+			System.out.println("Press S key to reverse to see the square.");
+			System.out.println("=====================================================================");
 		}
 
-		if (tutorial31stuff) {
+		if (tutorial31stuff)
+		{
 			//mesh = new Mesh();
-			material = new Material(new Texture("test.png"), new Vector3f(1, 1, 1), 1, 8);
-			shader = PhongShader.getInstance();
-			camera = new Camera();
-			transform = new Transform();
+			tutorial31Material = new Material(new Texture("test.png"), new Vector3f(1, 1, 1), 1, 8);
+			tutorial31Shader = PhongShader.getInstance();
+			tutorial31Camera = new Camera();
+			tutorial31Transform = new Transform();
 
 			/*
 			Vertex[] data = new Vertex[] {
@@ -129,11 +157,11 @@ public class Game
 					2, 1, 3
 			};
 
-			mesh = new Mesh(vertices, indices, true);
+			tutorial31Mesh = new Mesh(vertices, indices, true);
 			//	mesh = new Mesh("box.obj");
 
-			transform.setProjection(70, Window.getWidth(), Window.getHeight(), 0.1f, 1000);
-			Transform.setCamera(camera);
+			tutorial31Transform.setProjection(70, Window.getWidth(), Window.getHeight(), 0.1f, 1000);
+			Transform.setCamera(tutorial31Camera);
 
 			PhongShader.setAmbientLight(new Vector3f(0.2f, 0.2f, 0.2f));
 			PhongShader.setDirectionalLight(
@@ -142,19 +170,21 @@ public class Game
 							new Vector3f(1f, 1f, 1f)));
 
 
-			PhongShader.setPointLights(new PointLight[]{pLight1, pLight2});
-			PhongShader.setSpotLights(new SpotLight[]{sLight1});
+			PhongShader.setPointLights(new PointLight[]{tutorial31PLight1, tutorial31PLight2});
+			PhongShader.setSpotLights(new SpotLight[]{tutorial31SLight1});
 		}
 	}
 	
 	public void input()
 	{
-		if (wolfenstein3Dclone) {
-
+		if (wolfenstein3Dclone)
+		{
+			Transform.getCamera().input();
 		}
 
-		if (tutorial31stuff) {
-			camera.input();
+		if (tutorial31stuff)
+		{
+			tutorial31Camera.input();
 			//if (Input.getKeyDown(Keyboard.KEY_UP))
 			//	System.out.println("We've just pressed up!");
 			//if (Input.getKeyUp(Keyboard.KEY_UP))
@@ -170,40 +200,46 @@ public class Game
 	
 	public void update()
 	{
-		if (wolfenstein3Dclone) {
+		if (wolfenstein3Dclone)
+		{
 
 		}
 
-		if (tutorial31stuff) {
+		if (tutorial31stuff)
+		{
 			temp += Time.getDelta();
 
 			//float sinTemp = (float)Math.sin(temp);
 
-			transform.setTranslation(0, -1f, 5);
+			tutorial31Transform.setTranslation(0, -1f, 5);
 
-			pLight1.setPosition(new Vector3f(3, 0, 8.0f * (float) (Math.sin(temp) + 1.0 / 2.0) + 10));
-			pLight2.setPosition(new Vector3f(7, 0, 8.0f * (float) (Math.cos(temp) + 1.0 / 2.0) + 10));
+			tutorial31PLight1.setPosition(new Vector3f(3, 0, 8.0f * (float) (Math.sin(temp) + 1.0 / 2.0) + 10));
+			tutorial31PLight2.setPosition(new Vector3f(7, 0, 8.0f * (float) (Math.cos(temp) + 1.0 / 2.0) + 10));
 
 			//transform.setRotation(0, sinTemp * 180, 0);
 			//float sc = 1.0f;
 			//transform.setScale(sc, sc, sc);
 
-			sLight1.getPointLight().setPosition(camera.getPos());
-			sLight1.setDirection(camera.getForward());
+			tutorial31SLight1.getPointLight().setPosition(tutorial31Camera.getPos());
+			tutorial31SLight1.setDirection(tutorial31Camera.getForward());
 		}
  	}
 	
 	public void render()
 	{
-		if (wolfenstein3Dclone) {
-
-		}
-
-		if (tutorial31stuff) {
-			RenderUtil.setClearColor(Transform.getCamera().getPos().div(2048f).abs());
+		if (wolfenstein3Dclone)
+		{
 			shader.bind();
 			shader.updateUniforms(transform.getTransformation(), transform.getProjectedTransformation(), material);
 			mesh.draw();
+		}
+
+		if (tutorial31stuff)
+		{
+			RenderUtil.setClearColor(Transform.getCamera().getPos().div(2048f).abs());
+			tutorial31Shader.bind();
+			tutorial31Shader.updateUniforms(tutorial31Transform.getTransformation(), tutorial31Transform.getProjectedTransformation(), tutorial31Material);
+			tutorial31Mesh.draw();
 
 			//Window.render();
 		}
@@ -211,11 +247,13 @@ public class Game
 	
 	public void cleanUp()
 	{
-		if (wolfenstein3Dclone) {
+		if (wolfenstein3Dclone)
+		{
 
 		}
 
-		if (tutorial31stuff) {
+		if (tutorial31stuff)
+		{
 			Window.dispose();
 		}
 	}
