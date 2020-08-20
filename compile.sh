1e2b0013ad83a2228e961ad76a1575b6f9623f9f
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2020-08-20 17:54:46 +0300

    Added command line shell scripts to compile and run project.

---
+++ compile.sh
@@ -0,0 +1,1 @@
+javac -cp .:res:lib/lwjgl.jar:lib/lwjgl_util.jar:lib/slick-util.jar:   src/com/base/engine/*/*.java src/com/base/game/*.java
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp .:res:lib/lwjgl.jar:lib/lwjgl_util.jar:lib/slick-util.jar:lib/PNGDecoder.jar:src: -Djava.library.path=natives/  com.base.engine.core.MainComponent
